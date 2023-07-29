2124b682275a14d98d92fb0ec8d0cb09f3df6ba5
Pёtr Kolesnikov <111289146+pklsnkov@users.noreply.github.com>
2023-07-29 07:44:05 +0300

    добавлена обрезка tiff'a по границе, загрузка в NGW (не полностью)

--- opetarions.py
+++ opetarions.py
@@ -12,9 +12,9 @@ boundary = 'boundary.geojson'
 catalog = ''
 platform = ''
 satt_imagery_type = ''
-webgis_addr = ''
-webgis_username = ''
-webgis_password = ''
+webgis_addr = 'https://kolesnikov-p.nextgis.com'
+webgis_username = 'pvk200815@gmail.com'
+webgis_password = 'yNCY3VQ4zNDDYJ4'
 download_directory = 'images'
 
 service_name = 'copernicus'  # захардкодено
@@ -47,36 +47,51 @@ scenes = ngss.search_by_conditions(platform='Sentinel-1',
 
 if scenes['code'] == 0:
     ids = scenes['data']
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        future_to_id = {executor.submit(ngss.download_by_identifier, identifier=id): id for id in ids}
 
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        future_to_id = {executor.submit(fn=ngss.download_by_identifier, identifier=id): id for id in ids}
         for future in concurrent.futures.as_completed(future_to_id):
             id = future_to_id[future]
             try:
                 future.result()
             except Exception as e:
-                print(f"Error occurred for id {id}: {e}")
+                print(f"Ошибка для id: {id}: {e}")
 
     transform.extract(download_directory)
-    transform.transform_tiff(download_directory, polarization_type)
+    transform.transform_tiff(download_directory, boundary, polarization_type)
 
 else:
     print(scenes['message'])
 
-
-#///
-
 # загрузка данных в NGW (https://docs.nextgis.ru/docs_ngweb_dev/doc/developer/file_upload.html#multiple-file-upload)
 
-# upload_url = f'{webgis_addr}/api/component/file_upload/'
-# upload_dir = 'images_reproj'
-# creds = f"{webgis_username}:{webgis_password}"
-# headers = {
-#             'Accept': '*/*',
-#             'Authorization': 'Basic' + ' ' + base64.b64encode(creds.encode("utf-8")).decode("utf-8")
-#         }
-# response = requests.post(url=upload_url,
-#                          files=upload_files,
-#                          headers=headers)
+upload_url = f'{webgis_addr}/api/component/file_upload/'
+images_reproj_directory = 'transformed_image'
+creds = f"{webgis_username}:{webgis_password}"
+headers = {
+            'Accept': '*/*',
+            'Authorization': 'Basic' + ' ' + base64.b64encode(creds.encode("utf-8")).decode("utf-8")
+        }
 
+upload_dirs = [folder for folder in os.listdir(images_reproj_directory)]
 
+for upload_dir in upload_dirs:
+    upload_dir_path = os.path.join(images_reproj_directory, upload_dir)
+    upload_files = [os.path.join(upload_dir_path, file) for file in os.listdir(upload_dir_path)]
+
+    print('Загрузка началась')
+
+    for upload_file in upload_files:
+        with open(upload_file, 'rb') as file:
+            files = {'files[]': (upload_file, file)}
+            form_data = {
+                'name': [upload_file, file]
+            }
+            response = requests.post(url=upload_url,
+                                     files=files,
+                                     data=form_data,
+                                     headers=headers,)
+        if response.status_code == 200:
+            print('Загрузка завершена', upload_file)
+        else:
+            print(response.status_code)  # todo : возможно, потом надо создать растровый слой
--- transform.py
+++ transform.py
@@ -1,7 +1,9 @@
 from NGSatSearch.NGSatSearch import NGSatSearch
 import os
 import zipfile
-from osgeo import gdal, osr
+import json
+from osgeo import gdal, ogr, osr
+import pyproj
 
 import numpy as np
 
@@ -9,56 +11,74 @@ import xml.etree.ElementTree as ET
 
 
 def extract(folder):
-    file_list = os.listdir(folder)
-    for file in file_list:
-        filepath = os.path.join(folder, file)
-        with zipfile.ZipFile(filepath, 'r') as zip_open:
+    dir_list = os.listdir(folder)
+    for directory in dir_list:
+        dir_path = os.path.join(folder, directory)
+        with zipfile.ZipFile(dir_path, 'r') as zip_open:
             zip_open.extractall(folder)
-        os.remove(filepath)
+        os.remove(dir_path)
+    print('Распаковка успешна')
 
 
-def transform_tiff(folder, polarization_type=None):
-    tiff_files = []
+def transform_tiff(folder, boundary, polarization_type=None):
 
-    if not os.path.isdir('images_reproj'):
-        os.mkdir('images_reproj')
+    if not os.path.isdir('transformed_image'):
+        os.mkdir('transformed_image')
 
     for attribute_folder in os.listdir(folder):
         measurement_folder = os.path.join(folder, attribute_folder, 'measurement')
         if polarization_type is None:
             tiff_file = os.path.join(measurement_folder, os.listdir(measurement_folder)[0])
-
             tiff_file_name, tiff_file_ext = os.path.splitext(os.path.basename(tiff_file))
-
-            if not os.path.isdir(f"images_reproj\\{tiff_file_name}"):
-                os.mkdir(f"images_reproj\\{tiff_file_name}")
-
-            output_file = os.path.join('images_reproj', f'{tiff_file_name}', f'{tiff_file_name}_reproj{tiff_file_ext}')
         else:
             if polarization_type == 'vh':
-                pass  # todo : заглушки
-            elif polarization_type == 'vv':
-                pass
+                for file in os.listdir(measurement_folder):
+                    if '_vh_' in file:
+                        tiff_file = os.path.join(measurement_folder, file)
 
-        src_dataset = gdal.Open(tiff_file)
+                        tiff_file_name, tiff_file_ext = os.path.splitext(os.path.basename(tiff_file))
+            elif polarization_type == 'vv':
+                for file in os.listdir(measurement_folder):
+                    if '_vv_' in file:
+                        tiff_file = os.path.join(measurement_folder, file)
+                        tiff_file_name, tiff_file_ext = os.path.splitext(os.path.basename(tiff_file))
+
+        if not os.path.isdir(f"transformed_image\\{tiff_file_name.upper()}"):
+            os.mkdir(f"transformed_image\\{tiff_file_name.upper()}")
+
+        output_file = os.path.join('transformed_image', f'{tiff_file_name.upper()}', f'{tiff_file_name}_reproj.tiff')
+
+        print('Файлы выбраны')
+
+        source_dataset = gdal.Open(tiff_file)
         target_projection = 'EPSG:3857'
 
-        if src_dataset is None:
+        if source_dataset is None:
             raise Exception("Не удалось открыть исходный GeoTIFF.")
 
-        src_srs = src_dataset.GetGCPProjection()
-        gcps = src_dataset.GetGCPs()
+        src_srs = source_dataset.GetGCPProjection()
+        gcps = source_dataset.GetGCPs()
 
         translate_options = gdal.TranslateOptions(outputSRS=target_projection, GCPs=gcps)
 
-        gdal.Translate(output_file, src_dataset, options=translate_options)
+        gdal.Translate(output_file, source_dataset, options=translate_options)
 
         # tiff_files.append(output_file)
+
+        source_dataset = None
+
+        print('Перепроецирование завершено')
+
+        # boundary_rep = reproject_geojson(boundary)
+        #
+        # cut_tiff = crop_tiff(output_file, boundary_rep, f"transformed_image\\{tiff_file_name.upper()}")
 
         channel_stat = calculating_percentiles(output_file)
         print(channel_stat)
 
-        qml_generator(channel_stat, f"images_reproj\\{tiff_file_name}")
+        # os.remove(cut_tiff)
+
+        qml_generator(channel_stat, f"transformed_image\\{tiff_file_name.upper()}")
 
     # return output_file
 
@@ -77,16 +97,17 @@ def calculating_percentiles(tiff_file):
         per_95 = np.percentile(channel_data, 95)
         # channel_stat.append((os.path.basename(tiff_file), per_5, per_95))
         channel_stat = {
-            'tiff_file': f'{os.path.splitext(os.path.basename(tiff_file))[0]}',
-            'per_5': str(per_5).split('.')[0],
-            'per_95': str(per_95).split('.')[0],
+            'tiff_file': f"{os.path.basename(tiff_file).split('.')[0]}",
+            'per_5': str(int(per_5)),
+            'per_95': str(int(per_95)),
         }
+
+    dataset = None
 
     return channel_stat
 
-def qml_generator(channel_stat, folder):
-    # for stat in channel_stat:
 
+def qml_generator(channel_stat, folder):
     qml_tree = ET.parse('default_files\\default.qml')
     root = qml_tree.getroot()
 
@@ -103,8 +124,52 @@ def qml_generator(channel_stat, folder):
     return qml_tree.write(f"{folder}\\{channel_stat['tiff_file']}.qml", encoding='UTF-8')
 
 
+def crop_tiff(input_file, boundary, folder):
+    cut_tiff = os.path.join(folder, 'raster.tiff')
+
+    # gdal.SetConfigOption('CHECK_DISK_FREE_SPACE', 'FALSE')
+
+    ds = gdal.OpenEx(boundary)
+    layer = ds.GetLayerByIndex(0)
+    name = layer.GetName()
+
+    # ds_tiff = gdal.Open(input_file)
+
+    res = gdal.Warp(
+        cut_tiff,  # Результирующий обрезанный растр
+        input_file,  # Исходный ратср
+        format='GTiff',  # Формат выходного растра
+        cutlineDSName=boundary,  # Путь до векторного набора с маской
+        cutlineLayer=name,  # Имя слоя внутри векторного набора, пояснения ниже отдельно
+        cropToCutline=True
+        # Указание, что нужно не просто сбросить все пиксели вне маски в NoData, но и обрезать экстент растра по экстенту маски)
+    )
+
+    del res  # Вызываем деструктор, чтобы записать данные на диск
+
+    return cut_tiff
 
 
-# transform_tiff('images - Copy')
-# output_file = reproject_and_calculating_percentiles('images - Copy')
-# calculating_percentiles(output_file)
+def reproject_geojson(input_file):
+    source_crs = pyproj.CRS("EPSG:4326")
+    target_crs = pyproj.CRS("EPSG:3857")
+
+    project = pyproj.Transformer.from_crs(source_crs, target_crs, always_xy=True)
+
+    with open(input_file, 'r') as f:
+        data = json.load(f)
+
+    for feature in data['features']:
+        geometry = feature['geometry']
+
+        geometry['coordinates'] = project.transform(geometry['coordinates'][0][0], geometry['coordinates'][0][1])
+
+    output_file = input_file.replace('.geojson', '_reprojected.geojson')
+    with open(output_file, 'w') as f:
+        json.dump(data, f)
+
+    return output_file
+
+
+# extract('images')
+# transform_tiff('images', 'boundary.geojson')
